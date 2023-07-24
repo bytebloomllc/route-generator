e9a103f584d4cc9138f80d10627e975dd2cab98d
ChristopherSimard <chrissimard41@gmail.com>
2023-07-24 11:09:59 -0400

    Initial Commit

---
+++ route-generator/route_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  route_generatorApp.swift
+//  route-generator
+//
+//  Created by Christopher Simard on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct route_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
